5139b3fe7ffa3a2f0bdc8b49c5d75d799686e626
Filippa <filippabellange@live.se>
2022-12-13 19:35:23 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="landing-page">
+    <div class="btn-container">
+      <router-link class="link-button" v-slot="{ navigate }" custom :to="'/'">
+        <button class="btn" @click="navigate">Vidare till konsten</button>
+      </router-link>
+    </div>
+    <div class="text-container">
+      <h1>Diana Bellange</h1>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup lang="ts">
+import Button from "primevue/button";
+</script>
+<style scoped lang="scss">
+.landing-page {
+  background: url("./assets/Art&Design\ \(1\).gif") no-repeat center center
+    fixed;
+  background-size: cover;
+  height: 100vh;
+  @include flex(column, space-between, flex-start);
+
+  .btn-container {
+    width: 100%;
+    padding: 90px 0px 0px 90px;
+    @include flex(row, flex-start, flex-start);
+    .btn {
+      width: 250px;
+      cursor: pointer;
+      font-family: $base-font;
+      font-size: large;
+      padding: 20px;
+      border: none;
+      border-radius: 5px;
+      background-color: rgba(255, 255, 255, 0.57);
+      box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+        rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+    }
+  }
+
+  .text-container {
+    width: 100%;
+    padding: 0px 90px 90px 0px;
+
+    @include flex(row, flex-end, flex-end);
+
+    h1 {
+      font-family: $base-font;
+      font-weight: 100;
+      color: white;
+      font-size: 2.5rem;
+    }
+  }
 }
 </style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>heeeeeej</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
