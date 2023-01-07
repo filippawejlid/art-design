5ba2a4894c88e82a162a0a0ae1f35e3f5900c1af
Filippa <filippabellange@live.se>
2023-01-07 17:26:23 +0100

    add products page, fetch prods startpage admin

--- client/src/pages/admin/add.vue
+++ client/src/pages/admin/add.vue
@@ -1,10 +1,153 @@
 <template>
-  <div>add</div>
+  <div class="add">
+    <div class="card">
+      <form @submit.prevent="handleSubmit" class="p-fluid">
+        <div class="field">
+          <FileUpload
+            mode="basic"
+            name="demo[]"
+            url="./upload"
+            id="img"
+            chooseLabel="Välj en bild"
+          />
+        </div>
+        <div class="field">
+          <div class="p-float-label">
+            <InputText
+              type="text"
+              v-model="state.product.name"
+              :hasError="v$.product.name.$error"
+              :class="{ 'p-invalid': v$.product.name.$error }"
+              id="name"
+            />
+            <label for="name" :class="{ 'p-error': v$.product.name.$error }"
+              >Namn på produkt*</label
+            >
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="p-float-label">
+            <InputText
+              type="text"
+              v-model="state.product.description"
+              :hasError="v$.product.description.$error"
+              :class="{ 'p-invalid': v$.product.description.$error }"
+              id="description"
+            />
+            <label
+              for="description"
+              :class="{ 'p-error': v$.product.description.$error }"
+              >Beskrivning av produkt*</label
+            >
+          </div>
+        </div>
+        <div class="field">
+          <div class="p-float-label">
+            <InputNumber
+              v-model="state.product.price"
+              :hasError="v$.product.price.$error"
+              :class="{ 'p-invalid': v$.product.price.$error }"
+              id="price"
+            />
+            <label for="price" :class="{ 'p-error': v$.product.price.$error }"
+              >Pris på produkt*</label
+            >
+          </div>
+        </div>
+        <div class="field">
+          <div class="p-float-label">
+            <InputNumber
+              v-model="state.product.stock"
+              :hasError="v$.product.stock.$error"
+              :class="{ 'p-invalid': v$.product.stock.$error }"
+              id="stock"
+            />
+            <label for="stock" :class="{ 'p-error': v$.product.stock.$error }"
+              >Lagerstatus på produkt*</label
+            >
+          </div>
+        </div>
+        <Button label="Spara" class="button" type="submit"></Button>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FileUpload from "primevue/fileupload";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Button from "primevue/button";
+import { required, email } from "@vuelidate/validators";
+import { reactive } from "vue";
+import { useVuelidate } from "@vuelidate/core";
 
-<style scoped></style>
+const state = reactive({
+  product: {
+    name: "",
+    img: "",
+    price: undefined,
+    stock: undefined,
+    description: undefined,
+  },
+});
+const rules = {
+  product: {
+    name: { required },
+    price: { required },
+    stock: { required },
+    description: { required },
+  },
+};
+
+const v$ = useVuelidate(rules, state);
+
+const handleSubmit = async () => {
+  const isValid = await v$.value.$validate();
+  if (!isValid) return;
+  console.log(state);
+};
+</script>
+
+<style lang="scss" scoped>
+.add {
+  height: 90vh;
+  @include flex(row, center, center);
+
+  .card {
+    * {
+      font-family: $secondary-font;
+    }
+    width: 300px;
+    background-color: white;
+    padding: 50px;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+
+    .field {
+      margin-bottom: 1.5rem;
+
+      .img-label {
+        font-size: 12px;
+      }
+    }
+    form {
+      margin-top: 2rem;
+    }
+  }
+}
+
+:deep(.p-button-label) {
+  font-family: $secondary-font;
+}
+
+:deep(.p-fileupload-choose) {
+  background-color: white;
+  color: #565d63;
+  border: none;
+}
+</style>
 <route lang="yaml">
 meta:
   layout: adminLayout
--- client/src/pages/admin/start.vue
+++ client/src/pages/admin/start.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="start">start</div>
+  <div class="start">
+    <div class="products">
+      <div class="product" v-for="prod in products" :key="prod._id">
+        <div class="img">
+          <img src="{{prod.img}}" alt="Bild av {{prod.name}}" />
+        </div>
+        <div class="info">
+          <p class="name">{{ prod.name }}</p>
+          <p class="price">{{ prod.price }}</p>
+          <p class="stock">Lagerstatus: {{ prod.stock }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useProductsQuery from "~/composables/queries/useProductsQuery";
+
+const { data: products } = useProductsQuery();
+</script>
 
 <style scoped></style>
 <route lang="yaml">
