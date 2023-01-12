16674eb329186bce8f16e515b9a13eb8b442d2b2
Filippa <filippabellange@live.se>
2023-01-12 19:35:57 +0100

    admin logout test

--- client/cypress/e2e/admin.cy.ts
+++ client/cypress/e2e/admin.cy.ts
@@ -15,4 +15,13 @@ describe("it tests user purchase", () => {
     cy.get(".p-button").click();
     cy.url().should("include", "/admin");
   });
+  it("logs admin out", () => {
+    cy.visit("http://127.0.0.1:5173/products");
+    cy.get(".link").click();
+    cy.get("#username").type("DianaB");
+    cy.get("#password").type("hej123");
+    cy.get(".p-button").click();
+    cy.get(".p-tabmenuitem:nth-of-type(5)").click();
+    cy.url().should("not.include", "/admin");
+  });
 });
